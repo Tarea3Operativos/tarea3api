bef6d9ff99c599ec7d29267a9c1ca2a7ab709ddf
Roberto2504 <robertoaf19@gmail.com>
2018-06-14 08:31:39 -0600

    saving progress

--- server/api/repoUsers/model.js
+++ server/api/repoUsers/model.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate';
 
 const repoUsersSchema = new mongoose.Schema({
-  username : {type : String, required : true},
-  password : {type : String, required : true},
+  username   : {type : String, required : true},
+  password   : {type : String, required : true},
+  superAdmin : { type : Boolean, default : false },
 }, {
   timestamps : true,
 });
